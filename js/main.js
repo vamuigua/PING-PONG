facb035c545af1da812e926f6032818f22af77a6
Victor Allen <vamuigua@gmail.com>
2017-05-12 16:43:27 +0300

    Modified output of user in js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,13 @@ Numbers divisible by 3 become "ping"
 Numbers divisible by 5 become "pong"
 Numbers divisible by 3 and 5 become "pingpong"
 */
-var user = document.getElementById("num_user");
-//var btnActivator = document.activeElement("calculate");
-var get_result = document.getElementById("result");
+var user = document.getElementById("numUser");
+var ans = document.getElementById("showAns");
 
-/*btnActivator.addEventListener("click", function(){
-   alert("Dont Touch Me!"); 
-});*/
-user.addEventListener("input", result);
-function result(){
-    var one = user.value;
-    get_result.innerHTML = one;
-    
+user.addEventListener("input", getResult);
+
+function getResult(){
+    var toDisplay = parseFloat(user.value) || 0;
+    ans.innerHTML = "Your answer is: " + toDisplay;
+    console.log(toDisplay);  
 }
