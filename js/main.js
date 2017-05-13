95e0eeff5a953fc17f977e67df17e2827c35a893
Victor Allen <vamuigua@gmail.com>
2017-05-13 18:18:22 +0300

    Modified js PIN PONG calculation for console.log  function

--- js/main.js
+++ js/main.js
@@ -3,32 +3,36 @@ Numbers divisible by 3 become "ping"
 Numbers divisible by 5 become "pong"
 Numbers divisible by 3 and 5 become "pingpong"
 */
+
+//Ensures The whole page loads first befor the Function is executed
 $(document).ready(main);
 
-var ans = document.getElementById("showAns");
-
-
+//Function to calculate PING-PONG
 function calculate(user) {
     var user = document.getElementById("numUser").value;
     var user = parseFloat(user);
     var Result = document.getElementById("Result");
     //Calculation for Ping, Pong and Ping-Pong!
-    for (var i = 0; i < user; i++){
-        if (user % 3 === 0 && user % 5 === 0) {
-            Result.innerHTML = "PINGPONG!";
-            console.log('pingpong');
-        } else if (user % 3 === 0) {
-            Result.innerHTML = "PING!";
-            console.log('ping');
-        } else if (user % 5 === 0) {
-            Result.innerHTML = "PONG!";
-            console.log('pong');
-        } else {
-            Result.innerHTML = user + " :Not divisible by either 3 or 5";
-            console.log(user + " :Not divisible by either 3 or 5");
+    for(var i = 1; i <= user; i++){
+        if(i % 3===0 && i % 5===0){
+            Result.innerHTML = "PINGPONG";
+            console.log("PINGPONG");
+            continue;
         }
+        else if(i % 3===0){
+            Result.innerHTML = "PING";
+            console.log("PING");
+            continue;
+        }
+        else if(i % 5===0){
+            Result.innerHTML = "PONG";
+            console.log("PONG");
+            continue;
+        }
+        console.log(i);
     }
 }
+
 
 function main() {
     $('.Title').hide();
