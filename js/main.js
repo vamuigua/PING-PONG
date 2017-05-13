142d08ea3ed14e764bfb0ad10ef34c9ee3e9d700
Victor Allen <vamuigua@gmail.com>
2017-05-13 15:51:38 +0300

    Calculation function updated

--- js/main.js
+++ js/main.js
@@ -3,18 +3,39 @@ Numbers divisible by 3 become "ping"
 Numbers divisible by 5 become "pong"
 Numbers divisible by 3 and 5 become "pingpong"
 */
+$(document).ready(main);
 
-//FadeIn Function for Title
+var ans = document.getElementById("showAns");
+
+
+function calculate(user){
+    var user = document.getElementById("numUser").value;
+    var user = parseFloat(user);
+    for(var i=0; i <= user; i++){
+        if(user % 3 === 0){
+            console.log('ping');
+        }
+        else if(user % 5 === 0){
+            console.log('pong');
+        }
+        else if(user % 3 === 0 && user % 5 ===0){
+            console.log('pingpong')
+        }
+        else{
+            console.log(user + " :Not divisible by either 3 or 5");
+        }      
+    }
+}
+
 function main(){
     $('.Title').hide();
-    $('.Title').fadeIn(1000);
+    $('.Title').fadeIn(1500);
     $('#btn-click').on('click',function(){
-        $('#pot').toggle();
-        $('#btn-click').toggleClass('active-color');
+        $(this).toggleClass('active-color');
+        $(this).text('Ping Pong!');
     })
 }
 
-$(document).ready(main);
 /*
 var user = document.getElementById("numUser");
 var ans = document.getElementById("showAns");
